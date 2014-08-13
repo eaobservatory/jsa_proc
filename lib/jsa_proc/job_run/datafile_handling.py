1d42fb638c2df56e0395deec69c7ed13da350df1
Sarah F Graves <s.graves@jach.hawaii.edu>
2014-08-13 12:19:34 -1000

    job_run: fix typo

--- lib/jsa_proc/job_run/datafile_handling.py
+++ lib/jsa_proc/job_run/datafile_handling.py
@@ -57,7 +57,7 @@ def assemble_input_data_for_job(job_id, input_file_list):
 
     # Make the input directory if it doesn't exist. (Permissions?).
     if (not os.path.exists(input_directory)
-            it and not os.path.isdir(input_directory)):
+            and not os.path.isdir(input_directory)):
         os.mkdir(input_directory)
 
     # Create list of files to write to.
