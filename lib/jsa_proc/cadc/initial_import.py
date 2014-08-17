c663c96c33eb168c716b2f3a38993ea56ca29fba
Graham Bell <g.bell@jach.hawaii.edu>
2014-08-17 09:08:04 -1000

    Add routine for initial import from CADC

---
+++ lib/jsa_proc/cadc/initial_import.py
@@ -0,0 +1,96 @@
+# Copyright (C) 2014 Science and Technology Facilities Council.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from jsa_proc.cadc.dpdb import CADCDP
+from jsa_proc.cadc.dpstate import CADCDPState
+from jsa_proc.cadc.param import parse_cadc_param
+from jsa_proc.config import get_database
+from jsa_proc.error import JSAProcError
+
+logger = logging.getLogger(__name__)
+
+
+def import_from_cadcdp(dry_run=False, db=None, cadc=None):
+    """Perform an initial import of existing jobs from CADC.
+
+    Returns true on success.
+    """
+
+    # Prepare counters.
+    n_ok = 0
+    n_already = 0
+    n_err = 0
+
+    # Connect to databases, unless we are given specific access
+    # objects to use.
+    if cadc is None:
+        cadc = CADCDP()
+    if db is None:
+        db = get_database()
+
+    # Retrieve set of tags we already have.
+    tags = set((x.tag for x in db.find_jobs(location='CADC')))
+
+    logger.info('Beginning import from CADC' +
+                (' (DRY RUN)' if dry_run else ''))
+
+    for job in cadc.get_recipe_info():
+        logger.debug('Importing recipe instance %s', job.id_)
+
+        if job.tag in tags:
+            logger.warning('Tag %s already present (recipe instance %s)',
+                           job.tag, job.id_)
+            n_already += 1
+            continue
+
+        try:
+            state = CADCDPState.jsaproc_state(job.state)
+
+            info = parse_cadc_param(job.parameters)
+
+            logger.debug('Getting input files for recipe instance %s', job.id_)
+            input = cadc.get_recipe_input_files(job.id_)
+
+            if not dry_run:
+                logger.debug('Inserting job for recipe instance %s', job.id_)
+                job_id = db.add_job(tag=job.tag,
+                                    location='CADC',
+                                    mode=info.mode,
+                                    parameters=info.parameters,
+                                    input_file_names=input,
+                                    foreign_id=job.id_,
+                                    state=state)
+
+                logger.debug('Recipe instance %s inserted as job %i',
+                             job.id_, job_id)
+
+            else:
+                logger.debug('Skipping job insert due to dry run mode')
+
+            n_ok += 1
+
+        except JSAProcError:
+            logger.exception('Failed to import recipe instance %s', job.id_)
+
+            n_err += 1
+
+    logger.info('Done Importing from CADC')
+    logger.info('Imported:        %i', n_ok)
+    logger.info('Already present: %i', n_already)
+    logger.info('Errors:          %i', n_err)
+
+    return False if n_err else True
---
+++ test/test_cadc_import.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2014 Science and Technology Facilities Council.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from jsa_proc.cadc.dpdb import CADCDPInfo
+from jsa_proc.cadc.initial_import import import_from_cadcdp
+from jsa_proc.state import JSAProcState
+
+from .db import DBTestCase
+
+
+class DummyCADCDP:
+    def __init__(self, ris):
+        self.info = []
+        self.file = {}
+
+        for (info, input) in ris:
+            self.info.append(info)
+            self.file[info.id_] = input
+
+    def get_recipe_info(self):
+        return self.info
+
+    def get_recipe_input_files(self, id_):
+        return self.file.get(id_, [])
+
+
+class CADCImportTestCase(DBTestCase):
+    def test_dummy_cadc(self):
+        """Check that the dummy CADC DP object itself works!"""
+
+        ri5 = CADCDPInfo(5, 'S', 'tag-5', 'PARAM')
+        ri6 = CADCDPInfo(6, 'N', 'tag-6', 'PARAM')
+
+        cadc = DummyCADCDP([
+            (ri5, ['file1', 'file2']),
+            (ri6, ['file3', 'file4']),
+        ])
+
+        self.assertEqual(cadc.get_recipe_info(), [ri5, ri6])
+        self.assertEqual(cadc.get_recipe_input_files(6), ['file3', 'file4'])
+        self.assertEqual(cadc.get_recipe_input_files(7), [])
+
+    def test_import(self):
+        """Test whether the initial import function works."""
+
+        param1 = '-mode="obs" -drparameters="RECIPE_NAME"'
+        param2 = '-mode="night" -drparameters="ANOTHER_RECIPE_NAME"'
+
+        cadc = DummyCADCDP([
+            (CADCDPInfo(11, 'S', 'tag-11', param1), ['f_11_01']),
+            (CADCDPInfo(12, 'N', 'tag-12', param1), ['f_12_01', 'f_12_02']),
+            (CADCDPInfo(13, 'Q', 'tag-13', param2), ['f_13_01']),
+            (CADCDPInfo(14, 'Y', 'tag-14', param2), ['f_14_01', 'f_14_02']),
+        ])
+
+        # Check that dry-run mode really does nothing.
+        self.assertTrue(import_from_cadcdp(True, self.db, cadc))
+        self.assertEqual(self.db.find_jobs(), [])
+
+        # Now perform real import.
+        self.assertTrue(import_from_cadcdp(False, self.db, cadc))
+
+        # See if we got all 4 recipe instances.
+        jobs = self.db.find_jobs()
+        self.assertEqual(len(jobs), 4)
+
+        # Organise data fetched from the database.
+        tag = {}
+        for job in jobs:
+            tag[job.tag] = (self.db.get_job(id_=job.id),
+                            sorted(self.db.get_input_files(job.id)))
+
+        # Check the retrieved information.
+        self.assertIn('tag-11', tag)
+        self.assertEqual(tag['tag-11'][0].location, 'CADC')
+        self.assertEqual(tag['tag-11'][0].foreign_id, '11')
+        self.assertEqual(tag['tag-11'][0].state, JSAProcState.RUNNING)
+        self.assertEqual(tag['tag-11'][0].mode, 'obs')
+        self.assertEqual(tag['tag-11'][0].parameters, 'RECIPE_NAME')
+        self.assertEqual(tag['tag-11'][1], ['f_11_01'])
+
+        self.assertIn('tag-12', tag)
+        self.assertEqual(tag['tag-12'][0].location, 'CADC')
+        self.assertEqual(tag['tag-12'][0].foreign_id, '12')
+        self.assertEqual(tag['tag-12'][0].state, JSAProcState.RUNNING)
+        self.assertEqual(tag['tag-12'][0].mode, 'obs')
+        self.assertEqual(tag['tag-12'][0].parameters, 'RECIPE_NAME')
+        self.assertEqual(tag['tag-12'][1], ['f_12_01', 'f_12_02'])
+
+        self.assertIn('tag-13', tag)
+        self.assertEqual(tag['tag-13'][0].location, 'CADC')
+        self.assertEqual(tag['tag-13'][0].foreign_id, '13')
+        self.assertEqual(tag['tag-13'][0].state, JSAProcState.QUEUED)
+        self.assertEqual(tag['tag-13'][0].mode, 'night')
+        self.assertEqual(tag['tag-13'][0].parameters, 'ANOTHER_RECIPE_NAME')
+        self.assertEqual(tag['tag-13'][1], ['f_13_01'])
+
+        self.assertIn('tag-14', tag)
+        self.assertEqual(tag['tag-14'][0].location, 'CADC')
+        self.assertEqual(tag['tag-14'][0].foreign_id, '14')
+        self.assertEqual(tag['tag-14'][0].state, JSAProcState.COMPLETE)
+        self.assertEqual(tag['tag-14'][0].mode, 'night')
+        self.assertEqual(tag['tag-14'][0].parameters, 'ANOTHER_RECIPE_NAME')
+        self.assertEqual(tag['tag-14'][1], ['f_14_01', 'f_14_02'])
