9c2d0dcf3b937c2a7f4704f63a9cd4538bfaf92f
Graham Bell <g.bell@jach.hawaii.edu>
2014-09-03 14:04:03 -1000

    Extract Sybase lock class
    Moved the lock class used for the CADC DP database into
    a separate file so that it can be used for other Sybase
    databases also.

--- lib/jsa_proc/cadc/dpdb.py
+++ lib/jsa_proc/cadc/dpdb.py
@@ -15,9 +15,8 @@
 
 from collections import namedtuple
 import Sybase
-from threading import Lock
 
-from jsa_proc.error import JSAProcError
+from jsa_proc.db.sybase import JSAProcSybaseLock
 from omp.siteconfig import get_omp_siteconfig
 
 cadc_dp_server = 'CADC_ASE'
@@ -29,65 +28,6 @@ jsa_tile_recipes = (
     'REDUCE_SCAN_JSA_PUBLIC',
     'REDUCE_SCIENCE_LEGACY',
 )
-
-
-class CADCDPLock:
-    """CADC DP database lock and cursor management class.
-    """
-
-    def __init__(self, conn):
-        """Construct object."""
-
-        self._lock = Lock()
-        self._conn = conn
-
-    def __enter__(self):
-        """Context manager block entry method.
-
-        Acquires the lock and provides a cursor.
-        """
-
-        self._lock.acquire(True)
-        self._cursor = self._conn.cursor()
-        return self._cursor
-
-    def __exit__(self, type_, value, tb):
-        """Context manager  block exit method.
-
-        Closes the cursor and releases the lock.  Since this module
-        is intended for read access only, it does not attempt to
-        commit a transaction.
-        """
-
-        # If we got a database-specific error, re-raise it as our
-        # generic error.  Let other exceptions through unchanged.
-        # Sybase appears to need us to read the error before
-        # closing the cursor?
-        new_exc = None
-        if type_ is None:
-            pass
-        elif issubclass(type_, Sybase.Error):
-            new_exc = JSAProcError(str(value))
-
-        try:
-            self._cursor.close()
-            del self._cursor
-        except Exception as e:
-            # Ignore errors trying to close the cursor if we are
-            # handling an exception, because Sybase can get into
-            # a state where we can't close it!
-            if type_ is None:
-                new_exc = JSAProcError('Failed to close cursor: ' + str(e))
-
-        self._lock.release()
-
-        if new_exc is not None:
-            raise new_exc
-
-    def close(self):
-        """Close the database connection."""
-
-        self._conn.close()
 
 
 class CADCDP:
@@ -109,7 +49,7 @@ class CADCDP:
             database=cadc_dp_db,
             auto_commit=0)
 
-        self.db = CADCDPLock(conn)
+        self.db = JSAProcSybaseLock(conn)
         self.recipe = None
 
     def __del__(self):
