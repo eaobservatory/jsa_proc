8c186e999eca0267f6139d0cce1e6a129599198e
Graham Bell <g.bell@jach.hawaii.edu>
2014-09-25 10:13:29 -1000

    Web: use jQuery to set checkbox status
    This avoids the need to iterate over the checkboxes manually.

--- web/static/js/select_all.js
+++ web/static/js/select_all.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
     $('#select_all').change(function (event) {
-      checkboxes = document.getElementsByName('job_id');
-      for(var i=0, n=checkboxes.length;i<n;i++) {
-        checkboxes[i].checked = event.target.checked;
-      }
+        $('input[type=checkbox]').prop('checked', event.target.checked);
     });
 });
