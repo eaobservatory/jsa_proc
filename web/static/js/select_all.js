edbbb30b7304c28673351692a209ce7e94cb32b5
Graham Bell <g.bell@jach.hawaii.edu>
2014-09-25 10:05:28 -1000

    Web: use change event for select all checkbox
    This is a better event to use than the basic clicked event.

--- web/static/js/select_all.js
+++ web/static/js/select_all.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#select_all').click(function (event) {
+    $('#select_all').change(function (event) {
       checkboxes = document.getElementsByName('job_id');
       for(var i=0, n=checkboxes.length;i<n;i++) {
         checkboxes[i].checked = event.target.checked;
