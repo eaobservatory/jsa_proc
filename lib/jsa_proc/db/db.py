eaobservatory/jsa_proc

commit 4a9a141b0c831d8001afc72a5ddd1a1e15c7c1b9

DB: diagnose failure setting state to current value
With MySQL we can get zero rows updated when setting the state to its
current value.  Therefore before raising an error about the lack
of rows being updated, check whether the reason for the failure
was that the job's state didn't need updating.

lib/jsa_proc/db/db.py
@@ -494,7 +494,15 @@ class JSAProcDB:
         c.execute(query, param)
 
         if c.rowcount == 0:
-            raise NoRowsError('job', query % tuple(param))
+            # (With MySQL) this might be because the job was already in the
+            # requested state.  Check this now (rather than before since
+            # we expect the state to be changing most times this method is
+            # called).
+            if self._get_job(c, 'id', job_id).state == newstate:
+                logger.warning('Job %i already in state %s', job_id, newstate)
+            else:
+                raise NoRowsError('job', query % tuple(param))
+
         elif c.rowcount > 1:
             raise ExcessRowsError('job', query % tuple(param))
 

test/test_db.py
@@ -274,7 +274,7 @@ class InterfaceDBTest(DBTestCase):
                              state_prev=JSAProcState.WAITING)
 
         with self.assertRaises(NoRowsError):
-            self.db.change_state(job_id, JSAProcState.RUNNING, 'test',
+            self.db.change_state(job_id, JSAProcState.FETCHING, 'test',
                                  state_prev=JSAProcState.WAITING)
 
         # Check that an error is raised if the new state is bad.

commit b44b11253694819ae2ad19ee204de33a5053b1e7

DB: add prev_state check to change_state diagnosis
Commit 4a9a141b0c831d8001afc72a5ddd1a1e15c7c1b9 introduced a check
for failure to update a row when changing the state to its current
value.  However when performing this test we also need to be sure
that the reason for failure was not that the previous state did
not match if specified.

lib/jsa_proc/db/db.py
@@ -498,7 +498,8 @@ class JSAProcDB:
             # requested state.  Check this now (rather than before since
             # we expect the state to be changing most times this method is
             # called).
-            if self._get_job(c, 'id', job_id).state == newstate:
+            if ((self._get_job(c, 'id', job_id).state == newstate) and
+                    ((state_prev is None) or (state_prev == newstate))):
                 logger.warning('Job %i already in state %s', job_id, newstate)
             else:
                 raise NoRowsError('job', query % tuple(param))

test/test_db.py
@@ -277,6 +277,10 @@ class InterfaceDBTest(DBTestCase):
             self.db.change_state(job_id, JSAProcState.FETCHING, 'test',
                                  state_prev=JSAProcState.WAITING)
 
+        with self.assertRaises(NoRowsError):
+            self.db.change_state(job_id, JSAProcState.RUNNING, 'test',
+                                 state_prev=JSAProcState.WAITING)
+
         # Check that an error is raised if the new state is bad.
         with self.assertRaises(JSAProcError):
             self.db.change_state(job_id, '!', 'test bad state')