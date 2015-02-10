23935838eb41011fa5a55d9afee32996f1fd773c
Graham Bell <g.bell@jach.hawaii.edu>
2015-02-09 17:43:02 -1000

    Action: add routine to retrieve output data
    This routine is designed to ensure that all of the output files
    for a job which has been queued for re-ingestion are present in
    the job output directory.

--- lib/jsa_proc/action/fetch.py
+++ lib/jsa_proc/action/fetch.py
@@ -14,14 +14,18 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import os
 
 from jsa_proc.action.decorators import ErrorDecorator
 from jsa_proc.action.datafile_handling \
     import assemble_input_data_for_job, filter_file_list, \
     assemble_parent_data_for_job, write_input_list
+from jsa_proc.admin.directories import get_output_dir
+from jsa_proc.cadc.fetch import fetch_cadc_file
 from jsa_proc.config import get_config, get_database
 from jsa_proc.state import JSAProcState
-from jsa_proc.files import get_input_dir_space
+from jsa_proc.files import get_input_dir_space, get_output_dir_space, \
+    get_md5sum
 from jsa_proc.error import JSAProcError, NoRowsError
 
 logger = logging.getLogger(__name__)
@@ -149,3 +153,113 @@ def fetch_a_job(job_id, db=None, force=False, replaceparent=False):
     return job_id
 
 
+def fetch_output(job_id=None, location=None, task=None,
+                 dry_run=False, force=False):
+    """Output fetch function for use from scripts."""
+
+    logger.debug('Connecting to JSA processing database')
+    db = get_database()
+
+    # Find next job if not specified.
+    if job_id is None:
+        force = False
+
+        logger.debug('Looking for a job to fetch output data for')
+
+        jobs = db.find_jobs(
+            state=JSAProcState.INGEST_QUEUE, location=location, task=task,
+            prioritize=True, number=1, sort=True)
+
+        if jobs:
+            job_id = jobs[0].id
+
+        else:
+            logger.warning('Did not find a job to fetch output data for!')
+            return
+
+    _fetch_job_output(job_id, db=db, force=force, dry_run=dry_run)
+
+
+@ErrorDecorator
+def _fetch_job_output(job_id, db, force=False, dry_run=False):
+    """Private function to perform retrieval of job output files from CADC.
+    """
+
+    # Check we have sufficient disk space for fetching to occur.
+    output_space = get_output_dir_space()
+    required_space = float(get_config().get('disk_limit', 'fetch_min_space'))
+
+    if output_space < required_space:
+        logger.warning('Insufficient disk space: %f / %f GiB required',
+                       output_space, required_space)
+        return
+
+    logger.info('About to retreive output data for job %i', job_id)
+
+    # Change state from INGEST_QUEUE to INGEST_FETCH.
+    if not dry_run:
+        try:
+            db.change_state(
+                job_id, JSAProcState.INGEST_FETCH,
+                'Output data are being retrieved',
+                state_prev=(None if force else JSAProcState.INGEST_QUEUE))
+        except NoRowsError:
+            logger.error('Job %i cannot have output data fetched'
+                         ' as it not waiting for reingestion', job_id)
+            return
+
+    # Check state of output files.
+    output_dir = get_output_dir(job_id)
+    output_files = db.get_output_files(job_id, with_info=True)
+    missing_files = []
+
+    for file in output_files:
+        filename = file.filename
+        filepath = os.path.join(output_dir, filename)
+
+        if os.path.exists(filepath):
+            # If we still have the file, check its MD5 sum is correct.
+            if file.md5 == get_md5sum(filepath):
+                logger.debug('PRESENT: %s', filename)
+            else:
+                raise JSAProcError('MD5 sum mismatch for existing file {0}'
+                                   .format(filename))
+
+        else:
+            # Otherwise add it to the list of missing files.
+            logger.debug('MISSING: %s', filename)
+            missing_files.append(file)
+
+    # Are there any files we need to retrieve?
+    if missing_files:
+        for file in missing_files:
+            filename = file.filename
+            filepath = os.path.join(output_dir, filename)
+
+            if not dry_run:
+                if os.path.exists(output_dir):
+                    logger.debug('Directory %s already exists', output_dir)
+                else:
+                    logger.debug('Making directory %s', output_dir)
+                    os.makedirs(output_dir)
+
+                logger.info('Fetching file %s', filename)
+                fetch_cadc_file(filename, output_dir, suffix='')
+
+                if file.md5 == get_md5sum(filepath):
+                    logger.debug('MD5 sum OK: %s', filename)
+                else:
+                    raise JSAProcError('MD5 sum mismatch for fetched file {0}'
+                                       .format(filename))
+            else:
+                logger.info('Skipping fetch of %s (DRY RUN)', filename)
+
+    else:
+        logger.info('All output files are already present')
+
+    # Finally set the state to INGESTION.
+    if not dry_run:
+        db.change_state(
+            job_id, JSAProcState.INGESTION,
+            'Output data have been retrieved',
+            state_prev=JSAProcState.INGEST_FETCH)
