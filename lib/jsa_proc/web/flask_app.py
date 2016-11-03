eaobservatory/jsa_proc

commit f0c196cfc672af53baeaf7a4470ac6f5e6159c65

Web: use "enum" values for states
State codes shouldn't appear in the code directly.

lib/jsa_proc/web/flask_app.py
@@ -326,12 +326,13 @@ def create_web_app():
             for p in projects:
                 jobs = db.find_jobs(
                     obsquery={'project': p}, task='jcmt-nightly')
-                projdict[p] = [len(jobs),
-                               sum(1 for j in jobs if j.state == "E"),
-                               sum(1 for j in jobs if j.qa_state == 'B'),
-                               sum(1 for j in jobs if j.qa_state == 'Q'),
-                               sum(1 for j in jobs if j.qa_state == '?'),
-                               sum(1 for j in jobs if j.qa_state == 'G')]
+                projdict[p] = [
+                    len(jobs),
+                    sum(1 for j in jobs if j.state == JSAPROCState.ERROR),
+                    sum(1 for j in jobs if j.qa_state == JSAQAState.BAD),
+                    sum(1 for j in jobs if j.qa_state == JSAQAState.QUESTIONABLE),
+                    sum(1 for j in jobs if j.qa_state == JSAQAState.UNKNOWN),
+                    sum(1 for j in jobs if j.qa_state == JSAQAState.GOOD)]
         else:
             projects = None
 

commit 732a4953f8cf6dd758abc8780668538c5b0bb014

web: fix typo from commit f0c196cf which broke fop_summary page

lib/jsa_proc/web/flask_app.py
@@ -346,7 +346,7 @@ def create_web_app():
                     obsquery={'project': p}, task='jcmt-nightly')
                 projdict[p] = [
                     len(jobs),
-                    sum(1 for j in jobs if j.state == JSAPROCState.ERROR),
+                    sum(1 for j in jobs if j.state == JSAProcState.ERROR),
                     sum(1 for j in jobs if j.qa_state == JSAQAState.BAD),
                     sum(1 for j in jobs if j.qa_state == JSAQAState.QUESTIONABLE),
                     sum(1 for j in jobs if j.qa_state == JSAQAState.UNKNOWN),